d11ae192644e5db4bc0fa76ce7926baa1ea32a62
eyaab <abdelmoulaeya@yahoo.fr>
2019-03-30 16:17:56 +0100

    ho

--- screens/ParticipantPage.js
+++ screens/ParticipantPage.js
@@ -1,13 +1,21 @@
 import React,{component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Notifications} from 'expo';
 import { registerForPushNotificationsAsync} from '../services'
 
 
 export default class ParticipantPage extends React.Component {
 componentWillMount(){
   registerForPushNotificationsAsync() ;
+  Notifications.addListener(this.handleNotifications) ;
+  console.log('hoooo');
 }
+handleNotification = (notification) => 
 
+{
+  console.log('hey');
+  console.log(notification);
+}
   render() {
     return (
       <View style={styles.container}>
