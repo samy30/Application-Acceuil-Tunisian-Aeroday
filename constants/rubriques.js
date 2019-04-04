2913397ff9de4cc2994320bf142b2d8a92b75f56
eyaab <abdelmoulaeya@yahoo.fr>
2019-04-04 14:29:04 +0200

    hek

--- constants/rubriques.js
+++ constants/rubriques.js
@@ -1,29 +1,29 @@
 export default [
     {
       title: "Qu'est ce que se passe maintenant à l'Aeroday ?",
-      image: require('../assets/images/plane2.jpg'),
+      image: require('../assets/images/realTime.jpg'),
       horizontal: true,
     },
     {
       title: 'Trouvez notre programme détaillé ici',
-      image: require('../assets/images/realtime.png'),
+      image: require('../assets/images/ydin2.jpg'),
     },
     {
       title: 'Quelques informations concernant nos chalenges et nos axes ?',
-      image: require('../assets/images/realtime.png'),
+      image: require('../assets/images/drone3.jpg'),
     },
     {
       title: 'Votez pour votre Participant préféré en AirShow',
-      image: require('../assets/images/airshow.jpg'),
+      image: require('../assets/images/hey2.jpg'),
       horizontal: true,
     },
     {
       title: 'Espace des participants aux compétitions',
-      image: require('../assets/images/realtime.png'),
+      image: require('../assets/images/h6.jpg'),
     },
     {
         title: 'Numéros Utiles',
-        image: require('../assets/images/realtime.png'),
+        image: require('../assets/images/realTime.jpg'),
       },
   ];
   
